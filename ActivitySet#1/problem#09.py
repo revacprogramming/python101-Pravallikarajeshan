c815b0512f1d4bd8fbf0fc7e9d2b8c9e1d34619c
Pravallikarajes <97019899+Pravallikarajeshan@users.noreply.github.com>
2022-05-14 17:50:05 +0000

    pc.9 done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,17 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+p=input("enter the number:")
+c=open(p)
+l=list()
+for line in c:
+    words=line.split()
+    for word in words:
+        if word in l:
+            continue
+        l.append(word)    
+   
+print(sorted(l))            
+
+    
+        
