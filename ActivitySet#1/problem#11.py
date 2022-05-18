fa0670e9cab590148930134bbf7f4a3d359feb38
Pravallikarajes <97019899+Pravallikarajeshan@users.noreply.github.com>
2022-05-18 17:27:12 +0000

    pc.11 done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,24 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+p=input("enter the file")
+c=open(p)
+x=tuple()
+
+
+for line in c:
+    if line.startswith ("From:"):
+        continue
+    word=line.split()
+    
+    counts=dict()   
+    for time in c:
+         counts[time]=counts.get(time,0)+1
+   
+    
+time=None
+hour=None
+
+for time,hours in counts.items():
+    x=sorted(time,hours)
+print(x)    
