4580b7e0efb7d861f4559760987f89d3dee1f0a3
Pravallikarajes <97019899+Pravallikarajeshan@users.noreply.github.com>
2022-05-22 15:44:21 +0000

    pc.a2 -1 done

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,17 @@
 
 
-def add(a, b):
-    return  # ...
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+c= a+b
+
+print("Sum of a and b is {sum(a, b)}",a,b,c)
 
 
-def main():
-    a = input  # ...
-    b = input  # ...
 
-    c = add(a, b)
-    print  # ...
+  
+  
+  
+
