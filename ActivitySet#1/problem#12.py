3e8a81f4ca1680c0d2e87c59af57791f47b0c37a
Pravallikarajes <97019899+Pravallikarajeshan@users.noreply.github.com>
2022-05-23 16:34:10 +0000

    PC 12 DONE

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,13 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+
+P = input("Enter file:")
+C = open(P)
+for line in C:
+    line=line.rstrip()
+    import re
+    line=re.findall(('[0-9]+'),line)
+    if len(num)<1:
+        continue
+    num=int(num)
+    line.append(line)
+    sum=num
+print(sum)
