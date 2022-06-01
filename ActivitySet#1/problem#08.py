7f0dcc6a2efdd5d97930e29f0eb800003ac6f8df
Pravallikarajes <97019899+Pravallikarajeshan@users.noreply.github.com>
2022-06-01 03:26:13 +0000

    p8 done

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,1 +1,18 @@
 # Files
+fname = input("Enter file name: ")
+fh =open(fname)
+count=0
+total=0
+for line in fh:
+    if not line.startswith("From:"):
+        continue
+        y=line.find(':')
+        #print(y)
+        x=line[y+1:]
+        #print(x)
+        z=float(x)
+        #print(z)
+        count=count+1
+        total=total+z
+average=total/count
+print("Average spam confidence:",aver
