8a31fb4ca0a78fde5ab6b98ffff921ba38d7fbe3
Pravallikarajes <97019899+Pravallikarajeshan@users.noreply.github.com>
2022-06-05 17:05:48 +0000

    done

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,12 +1,12 @@
 #databases
-CREATE TABLE yyy( 
+CREATE TABLE yy( 
   name VARCHAR(128), 
   age INTEGER
 );
-DELETE FROM yyy;
-INSERT INTO yyy (name, age) VALUES ('Amylee', 21);
-INSERT INTO yyy(name, age) VALUES ('Samy', 25);
-INSERT INTO yyy (name, age) VALUES ('Krish', 14);
-INSERT INTO yyy (name, age) VALUES ('Reggie', 27);
+DELETE FROM yy;
+INSERT INTO yy(name, age) VALUES ('Amylee', 21);
+INSERT INTO yy(name, age) VALUES ('Samy', 25);
+INSERT INTO yy (name, age) VALUES ('Krish', 14);
+INSERT INTO yy(name, age) VALUES ('Reggie', 27);
 INSERT INTO Age (name, age) VALUES ('Praise', 36);
-SELECT hex(name || age) AS X FROM yyy ORDER BY X
+SELECT hex(name || age) AS X FROM yy ORDER BY X
